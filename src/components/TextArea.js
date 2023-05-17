89dc53b722e946dee49affb51d3a6a44e0e5815d
himanshu2k23 <himanshudubey2k23@gmail.com>
2023-05-18 01:36:19 +0530

    added utils

---
+++ src/components/TextArea.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+
+export default function TextArea(props) {
+
+    const [text, setText] = useState("")
+    const handleOnChange = (event) => {
+        setText(event.target.value)
+    }
+    const handleUppercaseClick = () => {
+        setText(text.toUpperCase())
+    }
+    const handleLowercaseClick = () => {
+        setText(text.toLowerCase())
+    }
+    const clearTextarea = () => {
+        setText("")
+    }
+    const countWords=(text)=>{
+        let trimmedText=text.trim();
+        return text? trimmedText.split(" ").length : 0
+    }
+
+    return (
+        <>
+            <h1>{props.heading}</h1>
+            <div className="mb-3">
+                <textarea className="form-control" value={text} onChange={handleOnChange} id="floatingTextarea2" rows="8"></textarea>
+            </div>
+            <div className="d-flex">
+                <button className="btn btn-primary mx-2" onClick={handleUppercaseClick}>Uppercase</button>
+                <button className="btn btn-primary mx-2" onClick={handleLowercaseClick}>Lowercase</button>
+                <button className="btn btn-danger mx-2" onClick={clearTextarea}>Clear</button>
+            </div>
+            <div className="container my-3">
+                <p>{countWords(text)} words and {text.length} characters</p>
+                <h3>Preview:</h3>
+                <p>{text}</p>
+            </div>
+        </>
+    )
+}
