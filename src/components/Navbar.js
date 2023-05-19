170680417000d2c9acee775ce8191a0d1a571757
himanshu2k23 <himanshudubey2k23@gmail.com>
2023-05-19 21:27:40 +0530

    theme

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Navbar(props) {
+  const [theme, setTheme] = useState("light")
+  const changeTheme = () => {
+    let newtheme = theme === "light" ? "dark" : "light";
+    setTheme(newtheme);
+  }
   return (
     <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className={`navbar navbar-expand-lg navbar-${theme} bg-${theme}`}>
         <div className="container-fluid">
           <a className="navbar-brand" href="#">{props.title}</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -14,14 +19,11 @@ export default function Navbar(props) {
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="#">Home</a>
               </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">About</a>
-              </li>
             </ul>
-            {/* <form className="d-flex">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                <button className="btn btn-outline-success" type="submit">Search</button>
-            </form> */}
+            <div className="form-check form-switch">
+              <input className="form-check-input" type="checkbox" onClick={changeTheme} id="themeToggle" />
+              <label className={`form-check-label text-${theme === "light" ? "dark" : "light"}`} htmlFor="themeToggle" >Enable {theme === "light" ? "dark" : "light"} mode</label>
+            </div>
           </div>
         </div>
       </nav>
