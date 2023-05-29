b502697113a699c13ab96cf20487db791fdbf123
himanshu2k23 <himanshudubey2k23@gmail.com>
2023-05-29 09:57:09 +0530

    alert

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Navbar from "./components/Navbar";
 import TextArea from "./components/TextArea";
-import React, { useState } from 'react'
+import Alert from "./components/Alert";
+import React, { useState } from 'react';
 function App() {
   const [theme, setTheme] = useState("light");
+  const [alert,setAlert]= useState(null);
   const changeTheme = () => {
     let newtheme = theme === "light" ? "dark" : "light";
     let bgcolor= theme === "light" ? "#262729" : "white"
@@ -14,6 +16,7 @@ function App() {
     <>
       <div>
         <Navbar title="Text Utils" theme={theme} changeTheme={changeTheme} />
+        <Alert/>
         <div className={`container my-3 text-${theme === "light" ? "dark" : "light"} `}>
           <TextArea heading="Enter your text here" theme={theme}/>
         </div>
---
+++ src/components/Alert.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Alert() {
+    return (
+        <>
+            <div class="alert alert-success" role="alert">
+                A simple success alert—check it out!
+            </div>
+        </>
+    )
+}
+
+export default Alert
